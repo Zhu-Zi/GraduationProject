15e4cf25c76462c9274ca2daa4a562264b524ed7
Zhu-Zi <qyazhu@163.com>
2017-04-27 17:17:59 +0800

    Hub文件

---
+++ asp.net/GraduationProject/Hubs/WorkflowHub.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using System.Threading.Tasks;
+
+namespace GraduationProject
+{
+    public class WorkflowHub : Hub
+    {
+        public void Send(string message)
+        {
+            var context = GlobalHost.ConnectionManager.GetHubContext<WorkflowHub>();
+            context.Clients.All.clientAddBroadcastMessage(message);
+        }
+    }
+}
